6d581d0ea7fb869b41483c6cc61530b6157c730a
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-28 10:21:17 -0500

    Restored integration_spec

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
@@ -5,6 +5,7 @@ hqDefine("cloudcare/js/form_entry/spec/main", [
 ) {
     hqRequire([
         "cloudcare/js/form_entry/spec/case_list_pagination_spec",
+        "cloudcare/js/form_entry/spec/integration_spec",
         "cloudcare/js/form_entry/spec/utils_spec",
     ], function () {
         hqMocha.run();
