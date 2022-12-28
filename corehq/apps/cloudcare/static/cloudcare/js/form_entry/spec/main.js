0883cd01baac09dc61620e9e19483391bf16c3ed
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-28 10:14:43 -0500

    Restored case_list_pagination_spec

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
@@ -4,6 +4,7 @@ hqDefine("cloudcare/js/form_entry/spec/main", [
     hqMocha
 ) {
     hqRequire([
+        "cloudcare/js/form_entry/spec/case_list_pagination_spec",
     ], function () {
         hqMocha.run();
     });
