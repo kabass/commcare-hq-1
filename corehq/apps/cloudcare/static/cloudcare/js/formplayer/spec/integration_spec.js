08b62fc06367839de12ec2329a1c9d36e84cd8d0
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-28 09:53:03 -0500

    Added integration_spec back

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/integration_spec.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/integration_spec.js
@@ -1,70 +1,81 @@
-/* global Backbone */
 /* eslint-env mocha */
+hqDefine("cloudcare/js/formplayer/spec/integration_spec", [
+    "underscore",
+    "backbone",
+    "sinon/pkg/sinon",
+    "cloudcare/js/formplayer/app",
+    "cloudcare/js/formplayer/users/models",
+    "cloudcare/js/formplayer/utils/utils",
+], function (
+    _,
+    Backbone,
+    sinon,
+    FormplayerFrontend,
+    UsersModels,
+    Utils
+) {
+    describe('FormplayerFrontend Integration', function () {
+        describe('Start up', function () {
+            let options,
+                server;
+            beforeEach(function () {
+                server = sinon.useFakeXMLHttpRequest();
+                options = {
+                    username: 'batman',
+                    domain: 'domain',
+                    apps: [],
+                };
+                sinon.stub(Backbone.history, 'start').callsFake(sinon.spy());
 
-describe('FormplayerFrontend Integration', function () {
-    let FormplayerFrontend = hqImport("cloudcare/js/formplayer/app"),
-        UsersModels = hqImport("cloudcare/js/formplayer/users/models");
+                // Prevent showing views, which doesn't work properly in tests
+                FormplayerFrontend.off("before:start");
+                FormplayerFrontend.regions = {
+                    getRegion: function () {
+                        return {
+                            show: function () {
+                                return;
+                            },
+                        };
+                    },
+                };
+            });
 
-    describe('Start up', function () {
-        let options,
-            server;
-        beforeEach(function () {
-            server = sinon.useFakeXMLHttpRequest();
-            options = {
-                username: 'batman',
-                domain: 'domain',
-                apps: [],
-            };
-            sinon.stub(Backbone.history, 'start').callsFake(sinon.spy());
+            afterEach(function () {
+                server.restore();
+                Backbone.history.start.restore();
+            });
 
-            // Prevent showing views, which doesn't work properly in tests
-            FormplayerFrontend.off("before:start");
-            FormplayerFrontend.regions = {
-                getRegion: function () {
-                    return {
-                        show: function () {
-                            return;
-                        },
-                    };
-                },
-            };
-        });
+            it('should start the formplayer frontend app', function () {
+                FormplayerFrontend.start(options);
 
-        afterEach(function () {
-            server.restore();
-            Backbone.history.start.restore();
-        });
+                let user = UsersModels.getCurrentUser();
+                assert.equal(user.username, options.username);
+                assert.equal(user.domain, options.domain);
+            });
 
-        it('should start the formplayer frontend app', function () {
-            FormplayerFrontend.start(options);
+            it('should correctly restore display options', function () {
+                let newOptions = _.clone(options),
+                    user;
+                newOptions.phoneMode = true;
+                newOptions.oneQuestionPerScreen = true;
+                newOptions.language = 'sindarin';
 
-            let user = UsersModels.getCurrentUser();
-            assert.equal(user.username, options.username);
-            assert.equal(user.domain, options.domain);
-        });
+                FormplayerFrontend.start(newOptions);
 
-        it('should correctly restore display options', function () {
-            let newOptions = _.clone(options),
-                user;
-            newOptions.phoneMode = true;
-            newOptions.oneQuestionPerScreen = true;
-            newOptions.language = 'sindarin';
+                user = UsersModels.getCurrentUser();
+                Utils.saveDisplayOptions(user.displayOptions);
 
-            FormplayerFrontend.start(newOptions);
+                // New session, but old options
+                FormplayerFrontend.start(options);
+                user = UsersModels.getCurrentUser();
 
-            user = UsersModels.getCurrentUser();
-            hqImport("cloudcare/js/formplayer/utils/utils").saveDisplayOptions(user.displayOptions);
-
-            // New session, but old options
-            FormplayerFrontend.start(options);
-            user = UsersModels.getCurrentUser();
-
-            assert.deepEqual(user.displayOptions, {
-                phoneMode: undefined, // we don't store this option
-                singleAppMode: undefined,
-                landingPageAppMode: undefined,
-                oneQuestionPerScreen: true,
-                language: 'sindarin',
+                assert.deepEqual(user.displayOptions, {
+                    phoneMode: undefined, // we don't store this option
+                    singleAppMode: undefined,
+                    landingPageAppMode: undefined,
+                    oneQuestionPerScreen: true,
+                    language: 'sindarin',
+                });
             });
         });
     });
