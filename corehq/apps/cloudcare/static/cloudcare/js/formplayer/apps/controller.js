dimagi/commcare-hq

Removed appselect channel responses in favor of directly calling apps API
Having these as implicit dependencies is just annoying.

corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/controller.js
@@ -4,19 +4,20 @@ hqDefine("cloudcare/js/formplayer/apps/controller", [
     'cloudcare/js/formplayer/constants',
     'cloudcare/js/formplayer/app',
     'cloudcare/js/formplayer/layout/views/settings',
+    'cloudcare/js/formplayer/apps/api',
     'cloudcare/js/formplayer/apps/views',
-    'cloudcare/js/formplayer/apps/api'  // appselect:apps
 ], function (
     $,
     Backbone,
     constants,
     FormplayerFrontend,
     settingsViews,
+    AppsAPI,
     views
 ) {
     return {
         listApps: function () {
-            $.when(FormplayerFrontend.getChannel().request("appselect:apps")).done(function (appCollection) {
+            $.when(AppsAPI.getAppEntities()).done(function (appCollection) {
                 let apps = appCollection.toJSON();
                 let isIncompleteFormsDisabled = (app) => (app.profile.properties || {})['cc-show-incomplete'] === 'no';
                 let isAllIncompleteFormsDisabled = apps.every(isIncompleteFormsDisabled);
@@ -34,7 +35,7 @@ hqDefine("cloudcare/js/formplayer/apps/controller", [
          * Renders a SingleAppView.
          */
         singleApp: function (appId) {
-            $.when(FormplayerFrontend.getChannel().request("appselect:apps")).done(function () {
+            $.when(AppsAPI.getAppEntities()).done(function () {
                 var singleAppView = views.SingleAppView({
                     appId: appId,
                 });
@@ -42,7 +43,7 @@ hqDefine("cloudcare/js/formplayer/apps/controller", [
             });
         },
         landingPageApp: function (appId) {
-            $.when(FormplayerFrontend.getChannel().request("appselect:apps")).done(function () {
+            $.when(AppsAPI.getAppEntities()).done(function () {
                 var landingPageAppView = views.LandingPageAppView({
                     appId: appId,
                 });

kabass/commcare-hq-1

corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/controller.js
@@ -4,19 +4,20 @@ hqDefine("cloudcare/js/formplayer/apps/controller", [
     'cloudcare/js/formplayer/constants',
     'cloudcare/js/formplayer/app',
     'cloudcare/js/formplayer/layout/views/settings',
+    'cloudcare/js/formplayer/apps/api',
     'cloudcare/js/formplayer/apps/views',
-    'cloudcare/js/formplayer/apps/api'  // appselect:apps
 ], function (
     $,
     Backbone,
     constants,
     FormplayerFrontend,
     settingsViews,
+    AppsAPI,
     views
 ) {
     return {
         listApps: function () {
-            $.when(FormplayerFrontend.getChannel().request("appselect:apps")).done(function (apps) {
+            $.when(AppsAPI.getAppEntities()).done(function (apps) {
                 var appGridView = views.GridView({
                     collection: apps,
                 });
@@ -29,7 +30,7 @@ hqDefine("cloudcare/js/formplayer/apps/controller", [
          * Renders a SingleAppView.
          */
         singleApp: function (appId) {
-            $.when(FormplayerFrontend.getChannel().request("appselect:apps")).done(function () {
+            $.when(AppsAPI.getAppEntities()).done(function () {
                 var singleAppView = views.SingleAppView({
                     appId: appId,
                 });
@@ -37,7 +38,7 @@ hqDefine("cloudcare/js/formplayer/apps/controller", [
             });
         },
         landingPageApp: function (appId) {
-            $.when(FormplayerFrontend.getChannel().request("appselect:apps")).done(function () {
+            $.when(AppsAPI.getAppEntities()).done(function () {
                 var landingPageAppView = views.LandingPageAppView({
                     appId: appId,
                 });