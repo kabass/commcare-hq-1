053e8a0dcf5eb7e720fa60be6375391c3c4440a9
Jennifer Schweers <orange.jenny@gmail.com>
2023-01-17 11:47:06 -0500

    Moved display options functions into users models

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/app.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/app.js
@@ -308,7 +308,7 @@ hqDefine("cloudcare/js/formplayer/app", [
         });
 
         var savedDisplayOptions = _.pick(
-            FormplayerUtils.getSavedDisplayOptions(),
+            UsersModels.getSavedDisplayOptions(),
             Const.ALLOWED_SAVED_OPTIONS
         );
         user.displayOptions = _.defaults(savedDisplayOptions, {
